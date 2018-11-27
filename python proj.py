b021a20ee65640d49e2368f7014c460a3dddb88b
Anuprita-kaple <43983115+anuprita2000@users.noreply.github.com>
2018-11-27 12:31:10 +0530

    Add files via upload

---
+++ python proj.py
@@ -0,0 +1,26 @@
+current city=(dropdown)
+
+import easygui*
+mobile no:easygui.enterbox('enter ur mobile no')
+msg="enter your choice"
+title="available choices"
+choices=['part time job','annual job']
+choice=choicebox(msg,title)
+
+if choice=="part time job":
+ print('sites=www.justdail.com
+ 	             www.truehomejobs.com
+ 	             www.dailyonlinejobs.com)
+ 	             
+elif  choice=="annual job":
+skills=easygui.enterbox('enter skills you posses')
+easygui.msgbox("your skills are"+skills)
+
+industry=(drop down)
+
+experience=easygui.enterbox('enter no.of exp years')
+easygui.msgbox('you have an experience of'+experience)
+print www.indeed.co.in
+      www.naukri.com
+      www.timesjobs.com
+      https://:in.linkendin.com
